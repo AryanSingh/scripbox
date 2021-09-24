efee206f0d72b87b0bbaafe72eb396d9608909e3
aryan <aryan@sleektext.com>
2021-09-25 01:01:16 +0530

    wrote test conditions for home page

---
+++ src/screens/tests/HomeScreen.test.tsx
@@ -0,0 +1,5 @@
+// should contain text all challenges
+// all challenges should be rendered
+// there should be a button for creating new challenge
+// there should be a filter button, clicking on which should change filtering order
+
