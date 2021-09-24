abc8b4eac6cc06568d3f4e09086d911692392f1a
aryan <aryan@sleektext.com>
2021-09-25 01:55:56 +0530

    mockdata and some text in homescreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Grid, { GridSpacing } from '@material-ui/core/Grid';
-import {TextField, Button} from "@material-ui/core";
+import {Typography} from "@material-ui/core";
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       flexGrow: 1,
+      height: '100vh'
+    },
+    wrapper: {
+      // margin: 'auto'
     },
     paper: {
       height: 140,
@@ -27,6 +31,9 @@ const HomeScreen = (props:IProps) => {
 
   return(
     <Grid container className={classes.root}>
+      <Grid justifyContent='center' className={classes.wrapper} item container xs={12}>
+        <Typography variant="h3">All Challenges</Typography>
+      </Grid>
       HOme screen
     </Grid>
 
--- src/screens/tests/HomeScreen.test.tsx
+++ src/screens/tests/HomeScreen.test.tsx
@@ -1,5 +1,14 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+import HomeScreen from "../HomeScreen";
+
 // should contain text all challenges
 // all challenges should be rendered
 // there should be a button for creating new challenge
 // there should be a filter button, clicking on which should change filtering order
 
+test('home screen is rendered',  () => {
+  render(<HomeScreen />);
+  expect(screen.getByText(/all challenges/i)).toBeInTheDocument();
+});
