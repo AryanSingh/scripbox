f767552962407ecceeb3b296718de200baff64f0
aryan <aryan@sleektext.com>
2021-09-25 02:14:26 +0530

    challenges failing tests written

--- src/screens/tests/HomeScreen.test.tsx
+++ src/screens/tests/HomeScreen.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
 import HomeScreen from "../HomeScreen";
+import mockData from '../../data/mockData.json';
 
 // should contain text all challenges
 // all challenges should be rendered
@@ -12,3 +13,18 @@ test('home screen is rendered',  () => {
   render(<HomeScreen />);
   expect(screen.getByText(/all challenges/i)).toBeInTheDocument();
 });
+
+test('all challenges are rendered correctly', () => {
+  render(<HomeScreen/>);
+  mockData.challenges.forEach((challenge) => {
+    expect(screen.queryByText(challenge.title)).toBeInTheDocument();
+    expect(screen.queryByText(challenge.description)).toBeInTheDocument();
+    expect(screen.queryByText(challenge.createdAt)).toBeInTheDocument();
+    expect(screen.queryByText(challenge.upVotes)).toBeInTheDocument();
+    expect(screen.queryByText(challenge.downVotes)).toBeInTheDocument();
+    challenge.tags.forEach((tag) => {
+      expect(screen.queryByText(tag)).toBeInTheDocument();
+    })
+
+  })
+})
